afae82e0a699edac548c90b5584ddbbc775b9aeb
Mac Sweeny <macsweenydev@gmail.com>
2023-11-26 17:34:54 +1000

    initial commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,21 @@
+self.__RSC_SERVER_MANIFEST={
+  "node": {
+    "63777fcb8cfcfc76840a2b0419bacf6a067cc509": {
+      "workers": {
+        "app/account/page": "(sc_server)/./node_modules/.pnpm/next@13.4.3_@babel+core@7.22.1_react-dom@18.2.0_react@18.2.0/node_modules/next/dist/build/webpack/loaders/next-flight-action-entry-loader.js?actions=%5B%5B%22%2FUsers%2Fmacbook%2FDesktop%2FSweeny%20Studio%2Fsweeny-studio%2Fapp%2Faccount%2Fpage.tsx%22%2C%5B%22%24%24ACTION_1%22%2C%22%24%24ACTION_3%22%5D%5D%5D&__client_imported__=!"
+      },
+      "layer": {
+        "app/account/page": "sc_server"
+      }
+    },
+    "4d347507d7332233828501c36f24343a3dc5a3a0": {
+      "workers": {
+        "app/account/page": "(sc_server)/./node_modules/.pnpm/next@13.4.3_@babel+core@7.22.1_react-dom@18.2.0_react@18.2.0/node_modules/next/dist/build/webpack/loaders/next-flight-action-entry-loader.js?actions=%5B%5B%22%2FUsers%2Fmacbook%2FDesktop%2FSweeny%20Studio%2Fsweeny-studio%2Fapp%2Faccount%2Fpage.tsx%22%2C%5B%22%24%24ACTION_1%22%2C%22%24%24ACTION_3%22%5D%5D%5D&__client_imported__=!"
+      },
+      "layer": {
+        "app/account/page": "sc_server"
+      }
+    }
+  },
+  "edge": {}
+}

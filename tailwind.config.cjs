dcad1488c710db3b3a91287513b49bae0eb3f5a7
Mac Sweeny <macsweenydev@gmail.com>
2023-11-27 14:31:54 +1000

    feat: casestudies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,31 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        satoshi: ["Satoshi", ...defaultTheme.fontFamily.sans],
-        melodrama: ["Melodrama"],
+        satoshi: ['Satoshi', ...defaultTheme.fontFamily.sans],
+        melodrama: ['Melodrama'],
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
       },
       animation: {
-        marquee: "marquee 50s linear infinite",
-        spinSlow: "spin 4s linear infinite",
+        marquee: 'marquee 50s linear infinite',
+        spinSlow: 'spin 4s linear infinite',
+      },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       },
       keyframes: {
         marquee: {
           from: {
-            transform: "translateX(0)",
+            transform: 'translateX(0)',
           },
           to: {
-            transform: "translateX(calc(-100% - 2.5rem))",
+            transform: 'translateX(calc(-100% - 2.5rem))',
           },
         },
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 };

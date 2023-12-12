345346565b7b495e7827010386cb1124c4bdde5c
Mac Sweeny <macsweenydev@gmail.com>
2023-12-12 16:03:25 +1000

    Add AstroJS Node package and remove unused files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig, sharpImageService } from 'astro/config';
 
 // https://astro.build/config
@@ -12,4 +13,16 @@ export default defineConfig({
     domains: ['source.unsplash.com', 'images.unsplash.com'],
   },
   integrations: [tailwind(), mdx(), sitemap(), react()],
+  output: 'server',
+  adapter: vercel(),
+  vite: {
+    define: {
+      'process.env.TMDB_API_KEY': JSON.stringify(process.env.TMDB_API_KEY),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['@components/ui/button.astro'],
+    },
+  },
 });
